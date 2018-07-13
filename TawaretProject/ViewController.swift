d61fea5c16018a0f76a89a6ae1f7e4fe9ca5bc29
JNGApura <joao.apura@gmail.com>
2018-07-13 16:08:33 -0700

    Initial Commit

---
+++ TawaretProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TawaretProject
+//
+//  Created by João Nuno Gaspar Apura on 13/07/2018.
+//  Copyright © 2018 JNGApura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
